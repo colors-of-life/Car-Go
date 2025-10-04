3fd003b49459e3423d94f638fb428e3f28be75ff
Rishi18360 <rishiraj.omkar@gmail.com>
2025-10-04 15:07:47 +0530

    db set

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import homeRoute from "./routes/home.js";
 import dotenv from "dotenv";
+import hook from "./webhooks/webhook.js"
+import { initPool } from "./database/pool.js";
 
 dotenv.config();
+await initPool()
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use("/", homeRoute);
+app.use(hook);
 
 app.listen(PORT);
 console.log("http://localhost:3000");
